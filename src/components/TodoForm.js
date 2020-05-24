95b3d5cc397a3018020090ae42a13f4338db0692
Rodrigo Pedra Brum <rodrigo.pedra@gmail.com>
2020-05-24 05:07:02 -0300

    use props.children instead of passing button text as regular prop

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -7,7 +7,7 @@ const TodoForm = React.forwardRef((props, ref) => {
     return(
         <form className="todoInput" onSubmit={props.handleButtonSubmit}>
             <Input ref={ref} className="input" onChange={props.handlePendingItem} value={props.pendingItem}/>
-            <Button textButton={'Adicionar'} disabled={props.pendingItem.length === 0}/>
+            <Button disabled={props.pendingItem.length === 0}>Adicionar</Button>
         </form>
     );
 })
