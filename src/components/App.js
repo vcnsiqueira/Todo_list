f54520e80a7844d8cd7c04ed431eddf96cd4afc2
Rodrigo Pedra Brum <rodrigo.pedra@gmail.com>
2020-05-24 04:41:43 -0300

    add early return when adding todo item

--- src/components/App.js
+++ src/components/App.js
@@ -30,12 +30,13 @@ class App extends Component {
     handleButtonSubmit = event => {
         event.preventDefault();
         if(this.state.list.indexOf(this.state.pendingItem) > -1) {  // Verificando se a tarefa já existe na lista (se existir, não será permitido acrescentar)
-            alert('Essa tarefa já existe! Escreva outra tarefa')
-        } else{
-            this.setState({
-                list: [...this.state.list, this.state.pendingItem], // Atualizando a lista com o elemento submetido
-            });
+            return alert('Essa tarefa já existe! Escreva outra tarefa')
         }
+
+        this.setState({
+            list: [...this.state.list, this.state.pendingItem], // Atualizando a lista com o elemento submetido
+        });
+
         this.setState({
             pendingItem: ''
         });
@@ -50,11 +51,11 @@ class App extends Component {
     }
 
     handleDone = index => { // Função que atualiza a lista de tarefas realizadas
-        
+
         const newDoneElement = this.state.list.filter(item => {
             return this.state.list.indexOf(item) === index
         })
-        
+
         this.setState({
             doneList: [...this.state.doneList, newDoneElement],
             list: this.state.list.filter(item => {
@@ -75,10 +76,10 @@ class App extends Component {
         return(
             <div className="wrapper">
                 <Title title={'Lista de Tarefas'}/>
-                <TodoForm 
-                    pendingItem={this.state.pendingItem} 
+                <TodoForm
+                    pendingItem={this.state.pendingItem}
                     handlePendingItem={this.handlePendingItem}
-                    handleButtonSubmit={this.handleButtonSubmit}   
+                    handleButtonSubmit={this.handleButtonSubmit}
                 />
                 <TodoList list={this.state.list} handleRemove={this.handleRemove} handleDone={this.handleDone} />
                 <DoneList doneList={this.state.doneList} handleRemoveDone={this.handleRemoveDone}/>
